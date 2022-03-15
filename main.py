668a6434e93c6bc1f445a7c052ec6527ce884d1f
Lukas Gerlach <lukas.gerlach@smail.inf.h-brs.de>
2022-03-15 20:04:38 +0100

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 global_step = 0.5
-goal_number = 2
+goal_number = 10
 
 number_of_iterations = int(goal_number/global_step)
 
@@ -15,7 +15,7 @@ def global_func(val, time=1):
     return val
 
 x = np.linspace(0,goal_number,200)
-plt.plot(x,ziel_func(x))
+plt.plot(x,ziel_func(x),label="Zielfunktion")
 
 def euler_verfahren_r(step,val):
     return val + step * global_func(val)
@@ -24,7 +24,7 @@ def euler_verfahren():
     res_list = [start_value]
     for i in range(number_of_iterations):
         res_list.append(euler_verfahren_r(global_step,res_list[i]))
-    plt.plot(np.linspace(0,goal_number,number_of_iterations+1),res_list)
+    plt.plot(np.linspace(0,goal_number,number_of_iterations+1),res_list,label="euler_verfahren")
 
     
 def zwei_schritt_Adams_Bashforth_verfahren_r(step,val_1,val_2):
@@ -34,11 +34,11 @@ def zwei_schritt_Adams_Bashforth_verfahren():
     res_list = [start_value,euler_verfahren_r(global_step,start_value)]
     for i in range(number_of_iterations-1):
         res_list.append(zwei_schritt_Adams_Bashforth_verfahren_r(global_step,res_list[i+1],res_list[i]))
-        print(global_step,res_list[i+1],res_list[i])
-        print(res_list)
-    plt.plot(np.linspace(0,goal_number,number_of_iterations+1),res_list)
+    plt.plot(np.linspace(0,goal_number,number_of_iterations+1),res_list,label="zwei_schritt_Adams_Bashforth_verfahren")
 
 
 euler_verfahren()
 zwei_schritt_Adams_Bashforth_verfahren()
+plt.legend()
+plt.grid()
 plt.show()
