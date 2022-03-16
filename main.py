d04ef8a633f54e5d6ef3f037e5b164e95d5a00f2
Lukas Gerlach <lukas.gerlach@smail.inf.h-brs.de>
2022-03-16 19:21:54 +0100

    .

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import esv
 import msv
-
+from numpy import sin,cos,tan
 
 
 GLOBAL_STEP = 0.5
-GOAL_NUMBER = 10
+GOAL_NUMBER = 2
 
 NUMBER_OF_ITERATIONS = int(GOAL_NUMBER/GLOBAL_STEP)
 
@@ -22,9 +22,21 @@ def log_function(val):
 def log_ableitung(val):
     return 1/(val+1)
 
-ziel_func = log_function
+def sin_func(val):
+    return sin(val)
 
-ableitung = log_ableitung
+def cos_func(val):
+    return cos(val)
+
+def tang_func(val):
+    return tan(val)
+
+def tang_func_r(val):
+    return 1 + pow(val,2)
+
+ziel_func = euler_function
+
+ableitung = euler_ableitung
 
 START_VALUE = ziel_func(0)
 
@@ -36,7 +48,7 @@ if __name__ == "__main__":
     msv_res = msv.zwei_schritt_Adams_Bashforth_verfahren(number_of_iterations=NUMBER_OF_ITERATIONS,start_value=START_VALUE,step=GLOBAL_STEP,func=ableitung)
     plt.plot(np.linspace(0,GOAL_NUMBER,NUMBER_OF_ITERATIONS+1),esv_res,label="euler_verfahren")
     plt.plot(np.linspace(0,GOAL_NUMBER,NUMBER_OF_ITERATIONS+1),msv_res,label="zwei_schritt_Adams_Bashforth_verfahren")
-
+    
     plt.legend()
     plt.grid()
     plt.show()
